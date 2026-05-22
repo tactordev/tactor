53cef532fc13b8240f04774224d8d290b5347c66
Tactor <119103845+tactordev@users.noreply.github.com>
2026-05-22 11:43:08 +0100

    Finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,63 @@
-import Image from "next/image";
+"use client";
+import {
+  Search,
+  Calendar1,
+  Siren
+} from "lucide-react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 // avatar: https://i.ibb.co/SXNKT7SY/image.png
 // avatar decoration: https://cdn.discordapp.com/media/v1/collectibles-shop/1343751617345749012/animated
 
-function Section({ children, className }: { children: React.ReactNode; className?: string }) {
+function Shortcut({ className, url, icon, name }: { className?: string; url: string; icon: React.ReactNode; name: string }) {
+  const router = useRouter();
+
   return (
-    <div className={`flex flex-col w-full h-full section px-8 py-6 rounded-md shadow-lg ${className || ''}`}>
-      {children}
+    <div title={name} className={`group hover:cursor-pointer flex flex-col justify-center items-center w-full h-full section px-2 py-2 rounded-md shadow-lg ${className || ''} transition-all duration-200 hover:-translate-y-[0.5px] overflow-hidden`} onClick={() => { router.push(url) }}>
+      {icon}
     </div>
   )
 }
+
+function encodeURIComponent(str: string) {
+  return str.replace(/ /g, '+');
+}
 export default function Home() {
+  const [searchFocused, setSearchFocused] = useState<boolean>(false);
+  const router = useRouter();
+
+  function searchFocus(val: boolean) {
+    return setSearchFocused(val);
+  }
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      console.log("keydown", e.key, searchFocused);
+      if (!searchFocused) return;
+      if (e.key === "Enter") {
+        const searchInput = document.getElementById("search-input");
+        const query = (searchInput as HTMLInputElement)?.value;
+        router.push(`https://www.google.com/search?q=${encodeURIComponent(query || "")}`);
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [searchFocused]);
+
   return (
-    <main className="grid grid-cols-6 grid-rows-6 h-screen w-screen p-8 gap-4 overflow-y-hidden overflow-x-hidden scrollbar-hide">
-      <Section className="col-span-2 row-span-2 relative">
-        <div className="flex flex-row items-center gap-4">
-          <img src="https://i.ibb.co/SXNKT7SY/image.png" alt="avatar" className="rounded-full w-16 h-16" />
-          <div>
-            <h1 className="text-2xl font-semibold">👋 Hey, I&apos;m <span className="font-bold">Tactor</span></h1>
-            <h2>&nbsp;An aspiring student and software engineer.</h2>
-          </div>
-          <div className="absolute bottom-4 right-4 flex flex-row gap-4 items-center">
-            <a href="https://github.com/tactordev" target="_blank" rel="noopener noreferrer" >
-              <svg height="32" aria-hidden="true" data-component="Octicon" viewBox="0 0 24 24" version="1.1" width="32" data-view-component="true" className="octicon octicon-mark-github fill-[#272727]">
-                  <path d="M10.226 17.284c-2.965-.36-5.054-2.493-5.054-5.256 0-1.123.404-2.336 1.078-3.144-.292-.741-.247-2.314.09-2.965.898-.112 2.111.36 2.83 1.01.853-.269 1.752-.404 2.853-.404 1.1 0 1.999.135 2.807.382.696-.629 1.932-1.1 2.83-.988.315.606.36 2.179.067 2.942.72.854 1.101 2 1.101 3.167 0 2.763-2.089 4.852-5.098 5.234.763.494 1.28 1.572 1.28 2.807v2.336c0 .674.561 1.056 1.235.786 4.066-1.55 7.255-5.615 7.255-10.646C23.5 6.188 18.334 1 11.978 1 5.62 1 .5 6.188.5 12.545c0 4.986 3.167 9.12 7.435 10.669.606.225 1.19-.18 1.19-.786V20.63a2.9 2.9 0 0 1-1.078.224c-1.483 0-2.359-.808-2.987-2.313-.247-.607-.517-.966-1.034-1.033-.27-.023-.359-.135-.359-.27 0-.27.45-.471.898-.471.652 0 1.213.404 1.797 1.235.45.651.921.943 1.483.943.561 0 .92-.202 1.437-.719.382-.381.674-.718.944-.943"></path>
-              </svg>
-            </a>
-            <a href="mailto:tactor@proton.me" target="_blank" rel="noopener noreferrer" >
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail-icon lucide-mail"><path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/><rect x="2" y="4" width="20" height="16" rx="2"/></svg>
-            </a>
-          </div>
-        </div>
-      </Section>
-      <Section className="col-span-4 row-span-4">
-        <h1 className="text-xl font-bold">Projects</h1>
-      </Section>
-      <Section className="col-span-2 row-span-4">
-        <h1 className="text-xl font-bold">Experience</h1>
-      </Section>
-      <Section className="col-span-2 row-span-2">
-        <h1 className="text-xl font-bold">Education</h1>
-      </Section>
-      <Section className="col-span-2 row-span-2">
-        <h1 className="text-xl font-bold">Testimonials</h1>
-      </Section>
-      <p></p>
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-row bg-white px-2 py-1 rounded-2xl items-center justify-center group">
+        <Search className="text-gray-500 mt-0.5" size={15} />
+        <input autoComplete="off" type="text" className="text-base pr-4 pl-2 focus:group-bg-blue-200 focus:outline-none" placeholder="Search anything..." id="search-input" onFocus={() => {searchFocus(true)}} onBlur={() => { console.log("blurring..."); searchFocus(false)}} />
+      </div>
+      <div className="flex flex-row gap-4 mt-4">
+        <Shortcut url={"https://calendar.google.com/calendar/u/0/r/week"} icon={<Calendar1 size={20} className="text-gray-500 group-hover:text-blue-500 transition-colors duration-200" />} name="Calendar" />
+        <Shortcut url={"https://docs.google.com/document/d/1zIQiN5CP4gy-PXSuN_ipuA1aXX9pDPg000GPT44WoLQ/edit?usp=sharing"} icon={<Siren size={20} className="text-gray-500 group-hover:text-blue-500 transition-colors duration-200" />} name="NEA" />
+      </div>
     </main>
   );
 }
